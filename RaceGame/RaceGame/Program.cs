3dd826265dde5710ab32bc4a9f217b92c7540b74
chen1500 <chen1500@student.nhl.nl>
2015-10-12 12:34:36 +0200

    comment

--- RaceGame/RaceGame/Program.cs
+++ RaceGame/RaceGame/Program.cs
@@ -17,6 +17,7 @@ namespace RaceGame
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //you
         }
     }
 }
